b8e1645027e28ebe2d309977065df79647e6dd24
takahiroaraki <s01110002e1117.gmail.com>
2019-11-04 14:48:48 +0900

    ほんのランダム選出完了

--- app/src/main/java/com/example/bookselect/MainActivity.kt
+++ app/src/main/java/com/example/bookselect/MainActivity.kt
@@ -6,15 +6,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.bookselect.R.id.imageView
+import kotlinx.android.synthetic.*
 import kotlinx.android.synthetic.main.activity_main.*
 import org.json.JSONException
 import org.json.JSONObject
 import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStreamReader
+import java.lang.StringBuilder
 import java.net.HttpURLConnection
 import java.net.MalformedURLException
 import java.net.URL
+import java.util.*
+import java.util.concurrent.ThreadLocalRandom
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,44 +29,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-
         //ボタンがクリックされたらAPIを叩く（AsyncTaskクラスを使って、非同期処理を行う）
-        //今回は、isbnを検索条件にして検索する本をランダムで選出するため、ランダム化する処理も使う
         button.setOnClickListener {
-
-
-
-            //国会図書館
-            /*       HitAPITask().execute(http://iss.ndl.go.jp/api/sru?operation=searchRetrieve&query=isbn=9784101006017)*/
-
-        /*  https://api.openbd.jp/v1/coverage*/
-            //limit offset
-            //googleBooks いける
-            HitAPITask().execute("https://www.googleapis.com/books/v1/volumes?q=isbn:9784102003039")
-        /*    //openBD ISBNの全件検索ができる
-            HitAPITask().execute("https://api.openbd.jp/v1/get?isbn=9784101006017")*/
-
+            var apiUrl = StringBuilder()
+            apiUrl.append("https://api.openbd.jp/v1/get?isbn=")
+            var isbn = this.doRandom()
+            apiUrl.append(isbn)
+            HitAPITask().execute(apiUrl.toString())
+            /*    //openBD ISBNの全件検索ができる*/
+            /*  https://api.openbd.jp/v1/coverage*/
         }
-
-
-
-
-        }
-
-   inner class HitAPITask : AsyncTask<String, String, String>() {
+    }
+    inner class HitAPITask : AsyncTask<String, String, String>() {
         //APIを叩く
         override fun doInBackground(vararg params: String?): String? {
 
             var connection: HttpURLConnection? = null
             var reader: BufferedReader? = null
             val buffer: StringBuffer
-
-
-
             try {
-
-
                 val url = URL(params[0])
                 connection = url.openConnection() as HttpURLConnection
 
@@ -70,7 +58,7 @@ class MainActivity : AppCompatActivity() {
             var response=connection.responseCode as HttpURLConnection*/
                 //返ってきたデータを使えるようにする
                 val stream = connection.inputStream
-                reader = BufferedReader(InputStreamReader(stream))
+                reader = BufferedReader(InputStreamReader(stream,"UTF-8"))
                 buffer = StringBuffer()
                 var line: String?
                 while (true) {
@@ -81,34 +69,34 @@ class MainActivity : AppCompatActivity() {
                     }
                     buffer.append(line)
                 }
-
                 //json形式から値を取得する
-                val jsonText = buffer.toString()
+                val preJsonText:String = buffer.toString()
+                println("ジェイソンテキスト検証　"+preJsonText)
+                //でたらめなisbnによって書籍情報が取れなかった場合に、もう一度呼び出す
+                if (preJsonText == "[null]") {
+                   return null
+                }
+                val jsonText=preJsonText.substring(1,(preJsonText.length-1))
                 //jsonオブジェクトに格納
                 val parentJsonObj = JSONObject(jsonText)
-                println("検証　　　" + parentJsonObj)
+
                 //テキストに表示するオブジェクトをとってくる(上から、タイトル、出版日、あらすじ、著者、画像)
-                val itemsArrayJson=parentJsonObj.getJSONArray("items").getJSONObject(0)
                 val titleJson: String =
-                    itemsArrayJson.getJSONObject("volumeInfo").getString("title")
+                    parentJsonObj.getJSONObject("summary").getString("title")
                 val publishedDateJson: String =
-                    itemsArrayJson.getJSONObject("volumeInfo").getString("publishedDate")
-                   val discriptionJson:String=itemsArrayJson.getJSONObject("volumeInfo").getString("description")
+                    parentJsonObj.getJSONObject("summary").getString("pubdate")
                 val authorsJson =
-                    itemsArrayJson.getJSONObject("volumeInfo").getJSONArray("authors")
-                        .getString(0)
-                val imgJson=itemsArrayJson.getJSONObject("volumeInfo").getJSONObject("imageLinks").getString("thumbnail")
+                    parentJsonObj.getJSONObject("summary").getString("author")
+                val discriptionJson =
+                    parentJsonObj.getJSONObject("onix").getJSONObject("CollateralDetail").getJSONArray("TextContent")
+                        .getJSONObject(0).getString("Text")
+                var imgJson =
+                    parentJsonObj.getJSONObject("summary").getString("cover")
 
-
-
-
-
-
-                val text: String =
-                    "あなたが読むべき本は？！\n\nタイトル：『$titleJson』\n\n著者：$authorsJson\n出版年：$publishedDateJson\nあらすじ：$discriptionJson"
+                //取得した値を配列に格納しリターン
+                val text: String? =
+                    "\n\nタイトル：『$titleJson』\n\n著者：$authorsJson\n出版年：$publishedDateJson\nあらすじ：$discriptionJson$imgJson"
                 return text
-
-
             } catch (e: MalformedURLException) {
                 e.printStackTrace()
             } catch (e: IOException) {
@@ -125,109 +113,84 @@ class MainActivity : AppCompatActivity() {
                 } catch (e: IOException) {
                     e.printStackTrace()
                 }
-
             }
             return null
-
         }
-
-
-
 
         //結果をテキストビューに格納する.
         override fun onPostExecute(result: String?) {
+
             super.onPostExecute(result)
 
-            //処理が１行なので、省略
-            if (result == null) return
-            textView.text = result
-         /*   Picasso.with(imageView.context).load(result).into(imageView)*/
+            println("リゾルト検証" + result)
+            //リゾルトがnullなら、ボタンを押してリターン
+            if (result == null) {
+                button.callOnClick()
+                return
+            }
 
-            var matchImage:ImageView=findViewById(R.id.imageView)
-            var imgUrl:String="https://books.google.com/books/content?id=nR4mrgEACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api"
-            Glide.with(application).load(imgUrl).into(matchImage)
+            //引数resultの末尾に入っている画像イメージurlを抜き出す
+            var url: String? = "http"
+            var urlPoint: Int = result.toString().indexOf(url.toString())
+            var imgUrl:String?=null
+            var text:String?=null
 
+            //画像が入っていなかったらnoImage画像を、入っていたらその画像を入れる
+            if(urlPoint==-1){
+                text = result.toString()
+                textView.text = result
+                imageView.setImageResource(R.drawable.noimage)
+            }else{
+             imgUrl=result.substring(urlPoint)
+             text=result.substring(0,result.toString().indexOf(url.toString()))
+            //テキストビューに注入
+            textView.text = text
+            /*   Picasso.with(imageView.context).load(result).into(imageView)*/
+            //イメージビューに注入
+            var matchImage: ImageView = findViewById(R.id.imageView)
+            Glide.with(application).load(imgUrl).into(matchImage)}
 
         }
     }
 
-
-    }
-
-
-
-/*
-{
-    "kind": "books#volumes",
-    "totalItems": 1,
-    "items": [
-    {
-        "kind": "books#volume",
-        "id": "nR4mrgEACAAJ",
-        "etag": "CDtKcZGlu/Y",
-        "selfLink": "https://www.googleapis.com/books/v1/volumes/nR4mrgEACAAJ",
-        "volumeInfo": {
-        "title": "ジキルとハイド",
-        "authors": [
-        "ロバート・L. スティーヴンソン"
-        ],
-        "publishedDate": "2015-02-01",
-        "description": "ロンドンの高名な紳士、ジキル博士の家にある時からハイドという男が出入りし始めた。彼は肌の青白い小男で不愉快な笑みをたたえ、人にかつてない嫌悪、さらには恐怖を抱かせるうえ、ついに殺人事件まで起こしてしまう。しかし、実はジキルが薬物によって邪悪なハイドへと姿を変えていたのだった...。人間の心に潜む善と悪の葛藤を描き、二重人格の代名詞としても名高い怪奇小説。",
-        "industryIdentifiers": [
-        {
-            "type": "ISBN_10",
-            "identifier": "4102003037"
-        },
-        {
-            "type": "ISBN_13",
-            "identifier": "9784102003039"
+    //ISBNをランダムで出す
+    fun doRandom(): String {
+        //ランダムな12桁の数を生成
+        var numMap: MutableMap<Int, Int> = mutableMapOf(13 to 9, 12 to 7, 11 to 8, 10 to 4)
+        val random = Random
+        val preArray = arrayOf(2, 3, 4, 5, 6, 7, 8, 9)
+        for (i in preArray) {
+            val randomNum: Int = random.nextInt(10)
+            numMap.put(i, randomNum)
         }
-        ],
-        "readingModes": {
-        "text": false,
-        "image": false
-    },
-        "pageCount": 153,
-        "printType": "BOOK",
-        "maturityRating": "NOT_MATURE",
-        "allowAnonLogging": false,
-        "contentVersion": "preview-1.0.0",
-        "imageLinks": {
-        "smallThumbnail": "http://books.google.com/books/content?id=nR4mrgEACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api",
-        "thumbnail": "http://books.google.com/books/content?id=nR4mrgEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
-    },
-        "language": "ja",
-        "previewLink": "http://books.google.co.jp/books?id=nR4mrgEACAAJ&dq=isbn:9784102003039&hl=&cd=1&source=gbs_api",
-        "infoLink": "http://books.google.co.jp/books?id=nR4mrgEACAAJ&dq=isbn:9784102003039&hl=&source=gbs_api",
-        "canonicalVolumeLink": "https://books.google.com/books/about/%E3%82%B8%E3%82%AD%E3%83%AB%E3%81%A8%E3%83%8F%E3%82%A4%E3%83%89.html?hl=&id=nR4mrgEACAAJ"
-    },
-        "saleInfo": {
-        "country": "JP",
-        "saleability": "NOT_FOR_SALE",
-        "isEbook": false
-    },
-        "accessInfo": {
-        "country": "JP",
-        "viewability": "NO_PAGES",
-        "embeddable": false,
-        "publicDomain": false,
-        "textToSpeechPermission": "ALLOWED",
-        "epub": {
-        "isAvailable": false
-    },
-        "pdf": {
-        "isAvailable": false
-    },
-        "webReaderLink": "http://play.google.com/books/reader?id=nR4mrgEACAAJ&hl=&printsec=frontcover&source=gbs_api",
-        "accessViewStatus": "NONE",
-        "quoteSharingAllowed": false
-    },
-        "searchInfo": {
-        "textSnippet": "ロンドンの高名な紳士、ジキル博士の家にある時からハイドという男が出入りし始めた。彼は肌の青白い小男で不愉快な笑みをたたえ、人にかつてない嫌悪、さらには恐怖を抱か ..."
+        //マップを回して、チェックデジットを算出する
+        val array = arrayOf(2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13)
+        var totalNum: Int = 0
+        for (j in array) {
+            if (j % 2 == 0) {
+                var evenNum = (numMap.get(j) as Int) * 3 as Int
+                totalNum += evenNum
+            } else {
+                var oddNum = numMap.get(j) as Int as Int
+                totalNum += oddNum
+            }
+        }
+        //足し合わせた数字の1の位を取得し、チェックデジットを算出
+        var stringTotalNum = totalNum.toString()
+        var lastNum = stringTotalNum.substring(stringTotalNum.length - 1)
+        var checkNum = 10 - lastNum.toInt()
+        numMap.put(1, checkNum)
+
+        //マップを回して、StringのISBNを取得
+        var isbn = StringBuilder()
+        val numArray = arrayOf(13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1)
+        for (k in numArray) {
+            isbn.append(numMap.get(k).toString())
+        }
+        println("ＩＳＢＮの検証　　" + isbn.toString())
+
+        return isbn.toString()
+
     }
-    }
-    ]
 }
 
-
-*/
